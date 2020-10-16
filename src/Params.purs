13b4a9d9da6827db9d7056af0e5568fca5bb68d8
Alex Kelley <adkelley@users.noreply.github.com>
2020-10-16 08:07:38 -0700

    refactor validate jukugo

--- src/Params.purs
+++ src/Params.purs
@@ -12,6 +12,7 @@ import Data.String.Pattern (Pattern (..))
 import Data.Either (Either (..))
 import Data.Maybe (fromMaybe)
 import Data.Int.Parse (parseInt, toRadix)
+import Data.String.Regex (regex, test, parseFlags)
 
 import Types (RTKArgs)
 
@@ -22,17 +23,42 @@ getArgs = do
   xs <- argv
   pure $ drop 2 $ fromFoldable xs
   
--- | A RTK index should be > 0 and < 3001
+-- | A RTK index should be an integer > 0 and < 3001
 inRange :: String -> Either Error String
 inRange index = do
   let i = fromMaybe 0 $ parseInt index $ toRadix 10 
   if (i > 0 && i < 3001)
     then Right index
-    else Left "RTK index must be > 0 and < 3001"
+    else Left "RTK index must be integer > 0 and < 3001"
 
 
 validateIndices :: List String -> Either Error (List String)
-validateIndices xs = traverse inRange xs
+validateIndices = traverse inRange
+
+-- UNICODE RANGE : DESCRIPTION
+-- 
+-- 3000-303F : punctuation
+-- 3040-309F : hiragana
+-- 30A0-30FF : katakana
+-- FF00-FFEF : Full-width roman + half-width katakana
+-- 4E00-9FAF : Common and uncommon kanji
+-- 
+-- Non-Japanese punctuation/formatting characters commonly used in Japanese text
+-- 2605-2606 : Stars
+-- 2190-2195 : Arrows
+-- u203B     : Weird asterisk thing
+
+
+isKanji :: String -> Either Error String
+isKanji kanji = do 
+  expression <- regex "[\\u4E00-\\u9FAF]" $ parseFlags "g" 
+  if (test expression kanji)
+    then Right kanji
+    else Left "Invalid jukugo"
+
+
+validateJukugo :: List String -> Either Error (List String)
+validateJukugo = traverse isKanji
 
 
 mkArgs :: String -> Array String -> Either Error RTKArgs
@@ -45,11 +71,13 @@ splitNode (x : _) =
 
 cmdLineParser :: Effect (Either Error RTKArgs)
 cmdLineParser = do
-  args_ <- getArgs
-  pure $ case args_ of
+  args <- getArgs
+  pure $ case args of
     "-p" : rest -> mkArgs "primsToFrames" $ A.fromFoldable rest
-    "-k" : rest -> mkArgs "kanjiToKeywords" $ splitNode rest
-    "-i" : rest -> mkArgs "kanjiToIndices" $ splitNode rest 
+    "-k" : rest -> (validateJukugo rest) >>=
+                       (\xs -> mkArgs "kanjiToKeywords" $ splitNode xs)
+    "-i" : rest -> (validateJukugo rest) >>= 
+                     (\xs -> mkArgs "kanjiToIndices" $ splitNode xs) 
     "-f" : rest -> (validateIndices rest) >>= 
                       (\xs -> mkArgs "indicesToFrames" $ A.fromFoldable xs) 
     Nil -> Left $ "Usage: node index.js <cmd> <arguments>"
