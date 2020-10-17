e4d2ff17d2350b1d6e056679f94e154d21c409b6
Alex Kelley <adkelley@users.noreply.github.com>
2020-10-17 14:19:59 -0700

    utilize Data.Either.note in RTK.purs

--- src/RTK.purs
+++ src/RTK.purs
@@ -7,7 +7,7 @@ import Data.Array (elemIndex, index, intercalate, uncons
                   , zipWith, intersect, difference
                   , head, tail
                   )
-import Data.Either (Either (..))
+import Data.Either (Either (..), note)
 import Data.Maybe (Maybe (..), fromMaybe)
 import Data.String (Pattern (..), split, trim)
 import Data.Traversable (traverse)
@@ -27,40 +27,20 @@ search_rtk
 search_rtk query keys values =
   traverse (\x -> elemIndex x keys >>= index values) query
 
---rtk_result :: String -> Either Error String
---rtk_result errMsg result = 
---  case result of
---    Just s -> Right s
---    Nothing -> Left "Something went wrong"
-
--- | Given a query, keys and values, find the query's values
--- | and append them into a string using a separator. If there's
--- | and error then return the error message parameter
---query_rtk :: Query -> Keys -> Values -> String -> Either Error String
---query_rtk query keys values separator = 
---  rtk_result $ 
---    liftA1 (\xs -> intercalate separator xs) $ 
---    search_rtk query keys values
 
 kanjiToKeywords :: Query -> Kanji -> Keywords -> Either Error String
 kanjiToKeywords query kanji keywords = do
   let result = liftA1 (\xs -> intercalate ", " xs) $ 
                  search_rtk query kanji keywords
-  case result of
-    Just s -> Right s
-    Nothing -> Left "kanjiToKeywords error"
---  query_rtk query kanji keywords ", "
+  note "kanjiToKeywords error" result
 
 kanjiToIndices :: Query -> Kanji -> Indices -> Either Error String
 kanjiToIndices query kanji indices = do
   let result = liftA1 (\xs -> intercalate ", " xs) $ 
-               search_rtk query kanji indices
-  case result of
-    Just s -> Right s
-    Nothing -> Left "kanjiToIndices error"
---  query_rtk query kanji indices ", " 
+                 search_rtk query kanji indices
+  note "kanjiToIndices error" result
 
--- | given an collection of indices return the RTK frames for each
+-- | Given an collection of indices return the RTK frames for each
 -- | index as a string, otherwise return the error message parameter
 indicesToFrames :: Query -> Keys -> Values -> Either Error String
 indicesToFrames query indices kanji = do
@@ -71,6 +51,9 @@ indicesToFrames query indices kanji = do
     Nothing -> Left $ "indicesToFrames error"
 
 
+-- | Given an collection of RTK primitives return the RTK frames for all
+-- | kanji containings these primitives, otherwise return the error 
+-- | message parameter
 primsToFrames 
   :: Array String 
   -> Array String 
@@ -95,22 +78,3 @@ primsToFrames ps cs ks = Right $ go cs ks 1 ""
           in
             go tcs tks (i+1) (result <> s)
         Nothing -> "" -- we never reach here
-
--- implementation using folds
---prims 
---  :: Array String 
---  -> Array String 
---  -> Array String 
---  -> Array String
---  -> String
---prims ps cs ks is = go
---  where
---    components s = trim <$> split (Pattern ";") s
---    hasPrims xs = [] == (difference ps $ intersect ps xs)
---    frame :: Tuple (Array String) (Tuple String String) -> String
---    frame (Tuple c (Tuple k i)) = 
---      if (hasPrims c)
---        then k <> "[" <> i <> "]"
---        else ""
---    fs = zipWith (\x y -> components x /\ y) cs $ zip ks is
---    go = foldr (\x -> (<>) (frame x)) "" fs
