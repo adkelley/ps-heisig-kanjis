fe20376a0ec8ae8266bfc3cd9f3f2ec72acde7e2
Alex Kelley <adkelley@users.noreply.github.com>
2021-08-13 08:12:44 -0700

    Documentation

--- src/Types.purs
+++ src/Types.purs
@@ -2,6 +2,11 @@ module Types ( CmdArgs, Command(..), RTKData, UpdateData) where
 
 import Google.Auth (Client)
   
+-- | P2F : Primitives to Frames
+-- | I2F : Indices to Frames
+-- | K2K : Kanji to Keywords
+-- | K2I : Kanji to Indices
+-- | UC  : Update Components
 data Command =
   P2F | I2F | K2K | K2I | UC
 
@@ -17,6 +22,8 @@ type UpdateData =
   , value  :: String
   }
   
+
+-- | Data returned from batchGet
 type RTKData =
   { kanji :: Array String
   , indices :: Array String
